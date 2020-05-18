2148cc2ebe05b3cc0f4198f84bfa4cba0dd5add6
Greg Garnhart <gegarnhart@email.wm.edu>
2020-05-18 17:58:18 -0400

    Early tones palette https://colorhunt.co/palette/163564

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <a href="/recipe">see recipe page</a>
+      <a href="https://gregoryegarnhart.typeform.com/to/m7AI6C">Typeform</a>
     </div>
   );
 }
